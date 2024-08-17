07fc85d4176a9e98f6dc436cee4c20ccac06255b
rimasultana <rimasultana20225@gmail.com>
2024-08-17 22:55:48 +0600

    banner-section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        clifford: "#da373d",
-      },
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
     },
-  },
-};
+    plugins: [],
+  }
